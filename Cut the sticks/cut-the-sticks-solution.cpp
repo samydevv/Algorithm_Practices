4b06733ff567ac98adb874f346c494aa54185513
Samy <samyking.sb@gmail.com>
2021-02-28 01:41:12 +0200

    Cut The Sticks problem

---
+++ Cut the sticks/cut-the-sticks-solution.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the cutTheSticks function below.
+vector<int> cutTheSticks(vector<int> arr) {
+    int n = arr.size(),min,index;
+    vector<int> answer;
+    answer.push_back(n);
+    sort(arr.begin(),arr.end());
+    min=*min_element(arr.begin(), arr.end());
+    while(n>1){
+        index=-1;
+        for(auto& element : arr){
+            element -= min;
+            if (element < min && element<=0){
+                index++;
+            }
+        }
+        arr.erase(arr.begin() , arr.begin()+index+1);
+        min=*min_element(arr.begin(), arr.end());
+        n = arr.size();
+        if (n!=0)
+            answer.push_back(n);
+    }
+    return answer;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = cutTheSticks(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+
+        if (i != result.size() - 1) {
+            cout << "\n";
+        }
+    }
+
+    cout << "\n";
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
