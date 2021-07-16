46084798021ef5a053ae6931df8537f5f738efd9
Samy <samyking,sb@gmail.com>
2021-07-16 21:46:12 +0200

    Non-Divisible Subset Problem

---
+++ Non-Divisible Subset/Non-Divisible Subset_second_right .cpp.cpp
@@ -0,0 +1,108 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+
+string rtrim(const string &);
+
+vector<string> split(const string &);
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+int nonDivisibleSubset(int k, vector<int> s) {
+    int max_size=0;
+    vector<int> holder(k,0);
+    for (int i = 0; i < s.size(); ++i)
+        holder[s[i]%k]++;
+
+    if (k%2==0)
+        max_size += min(holder[k/2],1);
+
+    max_size += min(holder[0],1);
+
+    for (int j = 1; j < (k/2)+1; ++j) {
+        if (j != k-j)
+            max_size += max(holder[j],holder[k-j]);
+    }
+    return max_size;
+}
+
+int main() {
+
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string s_temp_temp;
+    getline(cin, s_temp_temp);
+
+    vector<string> s_temp = split(rtrim(s_temp_temp));
+
+    vector<int> s(n);
+
+    for (int i = 0; i < n; i++) {
+        int s_item = stoi(s_temp[i]);
+
+        s[i] = s_item;
+    }
+
+    int result = nonDivisibleSubset(k, s);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+            s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
